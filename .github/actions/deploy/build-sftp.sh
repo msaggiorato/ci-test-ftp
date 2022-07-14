c5743e555530e2680f4521ce6b5315fdaffeee90
Matias Saggiorato <matiassaggiorato@gmail.com>
2022-07-14 09:57:13 -0300

    Only do deployment if changes are detected

--- .github/actions/deploy/build-sftp.sh
+++ .github/actions/deploy/build-sftp.sh
@@ -10,5 +10,10 @@ touch "${DETAILS_FILE}"
 rsync --dry-run -rci "${TARGET_DIR}/" "${SOURCE_DIR}/" | cut -d" " -f2- | xargs -I{} echo "+ {}" | sed '/\/$/d' >> "${DETAILS_FILE}"
 rsync --dry-run -rci --delete --existing --ignore-existing "${TARGET_DIR}/" "${SOURCE_DIR}/" | cut -d" " -f2- | xargs -I{} echo "- {}" | sed '/\/$/d' >> "${DETAILS_FILE}"
 
-cat "${DETAILS_FILE}"
+if [ -s "${DETAILS_FILE}" ]; then
+	cat "${DETAILS_FILE}"
+	echo "::set-output name=is-empty::false"
+else
+	echo "::set-output name=is-empty::true"
+fi
 
